a1009502e670f3e4260b2eb5eb957b4e554b928a
Bear <bear.ranasinghe@gmail.com>
2021-04-14 19:58:07 -0400

    server.js set up

--- server.js
+++ server.js
@@ -14,3 +14,9 @@ app.use(express.static(path.join(_dirname, 'build')));
 app.get('/*', function(req, res) {
     res.sendFile(path.join(_dirname, 'build', 'index.html'));
 });
+
+const port = process.env.PORT || 3001;
+
+app.listen(port, function() {
+    console.log(`Express app running on port ${port}`)
+});
